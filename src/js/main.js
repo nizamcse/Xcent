21e2a76149056a2e9d86cb3d2d4df5a79835c9c4
bipreshsec <bipresh.sec@gmail.com>
2017-02-19 14:22:38 +0600

    L

--- src/js/main.js
+++ src/js/main.js
@@ -72,4 +72,296 @@ $(document).ready(function () {
         }
         console.log(ww);
     });
+
+
+    // OWL SIMPLE
+
+    function addanim(){
+        var img = $(".owl-carousel-simple").find(".current").find(".slide-item").css('background-image');
+        img = img.replace('url(','').replace(')','').replace(/\"/gi, "");
+        $(this).css({'background-image':'none','background-color':'red'});
+        var w = $(".slide-item").width();
+        var h = $(".slide-item").height();
+
+        var totalTiles = 9;
+        var divW = Math.floor(w/3);
+        var divH = Math.floor(h/3);
+        var tiles = [];
+        for ( var i = 0; i < 9; i++ ) {
+            tiles.push('<div class="tile"/>');
+        }
+        var tile2 = [];
+
+        $tiles = $( tiles.join('') );
+
+        $($tiles).css( {
+            width: divW,
+            height: divH,
+            backgroundImage: 'url('+ img +')'
+        } );
+        var x = 0;
+        var y = 0;
+        $tiles.each(function(k) {
+            var pos = $(this).position();
+            var trTime = 450*(k+1);
+            $(this).addClass("test").css( {
+                width: divW,
+                height: divH,
+                top: divH*y +'px',
+                left: divW*x +'px',
+                backgroundPosition: -divW*x +'px '+ -divH*y +'px',
+                transition: 'all '+trTime+'ms ease-in'
+            } );
+
+            x++;
+
+            if(x==3){
+                x=0;
+                y++;
+            }
+        });
+
+        $(".owl-carousel-simple").find(".current").find(".slide-item").html( $tiles );
+    }
+
+    $(".owl-carousel-simple").owlCarousel({
+        items:1,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        },
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        afterAction: function(el){
+            //remove class active
+            this
+                .$owlItems
+                .removeClass('active')
+
+            //add class active
+            this
+                .$owlItems //owl internal $ object containing items
+                .eq(this.currentItem + 1)
+                .addClass('active')
+        },
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        afterMove: function (elem) {
+            $(".owl-carousel-simple").find(".current").removeClass("current");
+            var current = this.$owlItems.eq(this.currentItem).addClass("current");
+
+            addanim();
+        }
+    });
+
+
+
+
+
+
 });
+
+$(document).ready(function () {
+    /*$(".owl-carousel-simple").find(".slide-item").each(function () {
+        var img = $(this).css('background-image');
+        img = img.replace('url(','').replace(')','').replace(/\"/gi, "");
+        $(this).css({'background-image':'none','background-color':'red'});
+        var w = $(".slide-item").width();
+        var h = $(".slide-item").height();
+
+        var totalTiles = 9;
+        var divW = Math.floor(w/3);
+        var divH = Math.floor(h/3);
+        var tiles = [];
+        for ( var i = 0; i < 9; i++ ) {
+            tiles.push('<div class="tile"/>');
+        }
+        var tile2 = [];
+
+        $tiles = $( tiles.join('') );
+
+        $($tiles).css( {
+            width: divW,
+            height: divH,
+            backgroundImage: 'url('+ img +')'
+        } );
+        var x = 0;
+        var y = 0;
+        $tiles.each(function(k) {
+            var pos = $(this).position();
+            var trTime = 450*(k+1);
+            $(this).addClass("test").css( {
+                width: divW,
+                height: divH,
+                top: divH*y +'px',
+                left: divW*x +'px',
+                backgroundPosition: -divW*x +'px '+ -divH*y +'px',
+                transform: 'scale(1)',
+                transition: 'all '+trTime+'ms ease-in'
+            } );
+
+            x++;
+
+            if(x==3){
+                x=0;
+                y++;
+            }
+        });
+
+        $(this).html( $tiles );
+
+    });*/
+
+});
+
+/*function TileSlide(){
+    var img = $(".owl-carousel-simple").find(".current").find(".slide-item").css('background-image');
+    img = img.replace('url(','').replace(')','').replace(/\"/gi, "");
+    $(this).css({'background-image':'none','background-color':'red'});
+    var w = $(".slide-item").width();
+    var h = $(".slide-item").height();
+
+    var totalTiles = 9;
+    var divW = Math.floor(w/3);
+    var divH = Math.floor(h/3);
+    var tiles = [];
+    for ( var i = 0; i < 9; i++ ) {
+        tiles.push('<div class="tile"/>');
+    }
+    var tile2 = [];
+
+    $tiles = $( tiles.join('') );
+
+    $($tiles).css( {
+        width: divW,
+        height: divH,
+        backgroundImage: 'url('+ img +')'
+    } );
+    var x = 0;
+    var y = 0;
+    $tiles.each(function(k) {
+        var pos = $(this).position();
+        var trTime = 450*(k+1);
+        $(this).addClass("test").css( {
+            width: divW,
+            height: divH,
+            top: divH*y +'px',
+            left: divW*x +'px',
+            backgroundPosition: -divW*x +'px '+ -divH*y +'px',
+            transition: 'all '+trTime+'ms ease-in'
+        } );
+
+        x++;
+
+        if(x==3){
+            x=0;
+            y++;
+        }
+    });
+
+    $(".owl-carousel-simple").find(".current").find(".slide-item").html( $tiles );
+}*/
+
+function slideSwitch() {
+
+    var $active = $('.zious-carousel').find(".slide-item").hasClass("active");
+
+
+    var w = $(".slide-item").width();
+    var h = $(".slide-item").height();
+
+    console.log($active);
+    if($active){
+        var $currItem = $('.zious-carousel').find(".active");
+        var $next;
+        if(!$currItem.is(':last-child'))
+        {
+            $next = $currItem.next();
+            $next.addClass('active').css({'z-index':99});
+            console.log("TGTG "+$next);
+        }
+        else{
+
+            $('.zious-carousel').find(".slide-item:first").addClass('active').css({'z-index':98});
+        }
+
+
+        $currItem.removeClass('active').css({'z-index':0}).find('.tile').remove();
+    }
+    else{
+        $('.zious-carousel').find(".slide-item:first").addClass('active').css({'z-index':99});
+    }
+
+
+    var $disItem = $(".zious-carousel").find(".active");
+    var $img = $(".zious-carousel").find(".active").css('background-image');
+
+    $img = $img.replace('url(','').replace(')','').replace(/\"/gi, "");
+    $disItem.css({'background-image':'none'});
+    setTimeout(function(){ $(".zious-carousel").find(".active").css('background-image','url('+ $img +')'); alert($img); },2000);
+
+    var w = $(".slide-item").width();
+    var h = $(".slide-item").height();
+
+    var totalTiles = 9;
+    var divW = Math.floor(w/3);
+    var divH = Math.floor(h/3);
+    var tiles = [];
+    for ( var i = 0; i < 9; i++ ) {
+        tiles.push('<div class="tile"/>');
+    }
+    var tile2 = [];
+
+    $tiles = $( tiles.join('') );
+
+    $($tiles).css( {
+        width: divW,
+        height: divH,
+        backgroundImage: 'url('+ $img +')'
+    } );
+    var x = 0;
+    var y = 0;
+    $tiles.each(function(k) {
+        var pos = $(this).position();
+        var trTime = 450*(k+1);
+        $(this).addClass("test").css( {
+            width: divW,
+            height: divH,
+            top: divH*y +'px',
+            left: divW*x +'px',
+            backgroundPosition: -divW*x +'px '+ -divH*y +'px',
+            transform: 'scale(1)',
+            transition: 'all '+trTime+'ms ease-in'
+        } );
+
+        x++;
+
+        if(x==3){
+            x=0;
+            y++;
+        }
+    });
+
+    $($disItem).html( $tiles );
+
+
+}
+
+
+
+
+$(document).ready(function(){
+    slideSwitch();
+    $(function() {
+        setInterval( "slideSwitch()", 3000 );
+    });
+});
+
